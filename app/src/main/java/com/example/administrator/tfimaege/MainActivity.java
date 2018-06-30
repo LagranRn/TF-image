fcbefbea0ef3c287cdfdf4bc91ba3bb77c2f8c20
kong1337 <kong1337@qq.com>
2018-06-30 13:09:42 +0800

    初步提交

---
+++ app/src/main/java/com/example/administrator/tfimaege/MainActivity.java
@@ -0,0 +1,239 @@
+package com.example.administrator.tfimaege;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Size;
+import android.view.KeyEvent;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.widget.ImageView;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TextureView tv;
+    private SurfaceView sv;
+    private ImageView iv;
+    private int width,height;
+    private String mCameraId = "0";
+    private ImageReader imageReader;
+    private Size previewSize;
+    private int RESULT_CODE_CAMERA=1;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession mPreviewSession; //相机捕获会话，
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private CaptureRequest mCaptureRequest;//捕获请求，定义输出缓冲区以及显示界面(TextureView或SurfaceView)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+        tv = findViewById(R.id.tv);
+        tv.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                MainActivity.this.width = width;
+                MainActivity.this.height = height;
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
+
+    }
+
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        setCameraCharacteristics(manager);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                String[] perms = {"android.permission.CAMERA"};
+                ActivityCompat.requestPermissions(MainActivity.this,perms, RESULT_CODE_CAMERA);
+
+            }else {
+                manager.openCamera(mCameraId, stateCallback, null);
+            }
+
+        } catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setCameraCharacteristics(CameraManager manager) {
+        try
+        {
+            // 获取指定摄像头的特性
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(mCameraId);
+            // 获取摄像头支持的配置属性
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            // 获取摄像头支持的最大尺寸
+            Size largest = Collections.max(Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),new CompareSizesByArea());
+            // 创建一个ImageReader对象，用于获取摄像头的图像数据
+            imageReader = ImageReader.newInstance(largest.getWidth(), largest.getHeight(), ImageFormat.JPEG, 2);
+            // 获取最佳的预览尺寸
+            previewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class), width, height, largest);
+        }
+        catch (CameraAccessException e)
+        {
+            e.printStackTrace();
+        }
+        catch (NullPointerException e)
+        {
+        }
+    }
+
+    // 为Size定义一个比较器Comparator
+    static class CompareSizesByArea implements Comparator<Size>
+    {
+        @Override
+        public int compare(Size lhs, Size rhs)
+        {
+            // 强转为long保证不会发生溢出
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() - (long) rhs.getWidth() * rhs.getHeight());
+        }
+    }
+
+    private static Size chooseOptimalSize(Size[] choices, int width, int height, Size aspectRatio)
+    {
+        // 收集摄像头支持的大过预览Surface的分辨率
+        List<Size> bigEnough = new ArrayList<>();
+        int w = aspectRatio.getWidth();
+        int h = aspectRatio.getHeight();
+        for (Size option : choices)
+        {
+            if (option.getHeight() == option.getWidth() * h / w &&
+                    option.getWidth() >= width && option.getHeight() >= height)
+            {
+                bigEnough.add(option);
+            }
+        }
+        // 如果找到多个预览尺寸，获取其中面积最小的
+        if (bigEnough.size() > 0)
+        {
+            return Collections.min(bigEnough, new CompareSizesByArea());
+        }
+        else
+        {
+            //没有合适的预览尺寸
+            return choices[0];
+        }
+    }
+
+    /**摄像头状态的监听*/
+    private CameraDevice.StateCallback stateCallback = new CameraDevice. StateCallback()
+    {
+        @Override
+        public void onOpened(CameraDevice cameraDevice){
+            MainActivity.this.cameraDevice = cameraDevice;
+            takePreview();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice cameraDevice)
+        {
+            MainActivity.this.cameraDevice.close();
+            MainActivity.this.cameraDevice = null;
+
+        }
+        @Override
+        public void onError(CameraDevice cameraDevice, int error)
+        {
+            cameraDevice.close();
+        }
+    };
+
+    /**
+     * 开始预览
+     */
+    private void takePreview() {
+        SurfaceTexture mSurfaceTexture = tv.getSurfaceTexture();
+        //设置TextureView的缓冲区大小
+        mSurfaceTexture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+        //获取Surface显示预览数据
+        Surface mSurface = new Surface(mSurfaceTexture);
+        try {
+            //创建预览请求
+            mCaptureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            // 设置自动对焦模式
+            mCaptureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            //设置Surface作为预览数据的显示界面
+            mCaptureRequestBuilder.addTarget(mSurface);
+            //创建相机捕获会话，第一个参数是捕获数据的输出Surface列表，第二个参数是CameraCaptureSession的状态回调接口，当它创建好后会回调onConfigured方法，第三个参数用来确定Callback在哪个线程执行，为null的话就在当前线程执行
+            cameraDevice.createCaptureSession(Arrays.asList(mSurface,imageReader.getSurface()),new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    try {
+                        //开始预览
+                        mCaptureRequest = mCaptureRequestBuilder.build();
+                        mPreviewSession = session;
+                        //设置反复捕获数据的请求，这样预览界面就会一直有数据显示
+                        mPreviewSession.setRepeatingRequest(mCaptureRequest, null, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN||keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+
+
+            //new Thread(new save()).start();
+            Bitmap bitmap = tv.getBitmap();
+            iv.setImageBitmap(bitmap);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
